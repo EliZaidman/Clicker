8a1fa1aa2a7ffd5a2dbb6c08e71e20fea595c62d
Chen5454 <74614370+Chen5454@users.noreply.github.com>
2022-03-23 18:37:34 +0200

    Update Click.cs
    just declaring var+funcations

--- Assets/Scripts/Click.cs
+++ Assets/Scripts/Click.cs
@@ -8,6 +8,18 @@ public class Click : MonoBehaviour
 
     public int _money;
     public TextMeshProUGUI _moneyText;
+
+    #region ShopVar
+
+    public int shop1prize;
+    public TextMeshProUGUI shop1text;
+
+    public int shop2prize;
+    public TextMeshProUGUI shop2text;
+
+
+    #endregion
+
     void Start()
     {
         
@@ -17,10 +29,27 @@ public class Click : MonoBehaviour
     void Update()
     {
         _moneyText.text = _money.ToString();
+
+        #region ShopArea
+
+        shop1text.text = "Shop Tier 1 " + shop1prize + " $/@";
+        shop2text.text = "Shop Tier 1 " + shop2prize + " $/@";
+
+        #endregion
     }
 
     public void RiseMoney()
     {
         _money++;
+    }
+
+    public void Shop1()
+    {
+
+    }
+
+    public void Shop2()
+    {
+
     }
 }
