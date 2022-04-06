b1637717119bb7df02d9653434c040a93da69d0a
Chen5454 <74614370+Chen5454@users.noreply.github.com>
2022-04-06 22:17:20 +0300

    Renamed
    Renamed

--- Assets/Scripts/IdleClicker.cs
+++ Assets/Scripts/IdleClicker.cs
@@ -9,45 +9,45 @@ public class IdleClicker : MonoBehaviour
 
     [SerializeField]
     public  TextMeshProUGUI _idleClickerText;
-    public TextMeshProUGUI _shdrudalCostPerSecText;
+    public TextMeshProUGUI _shdrudalPassiveCostText;
     public TextMeshProUGUI _shdrudelText;
 
 
     public double shdrudel;
 
 
-    public double shtrudalpersecond;
-    public int idleClickerUpgrade;
-    public double idleClickerUpgradeCost;
+    public double shtrudalPassive;
+    public int passiveUpgrade;
+    public double passiveUpgradeCost;
 
     void Start()
     {
-        idleClickerUpgradeCost = 25;
+        passiveUpgradeCost = 25;
     }
 
     // Update is called once per frame
     void Update()
     {
-        shtrudalpersecond = idleClickerUpgrade;
+        shtrudalPassive = passiveUpgrade;
 
         _shdrudelText.text = "Shtrudel: " + shdrudel.ToString("F0");
 
-        _shdrudalCostPerSecText.text = shtrudalpersecond.ToString("F0") + "Shtrudel/s";
+        _shdrudalPassiveCostText.text = shtrudalPassive.ToString("F0") + "Shtrudel/s";
 
-        _idleClickerText.text = "Idle Clicker Upgrade 1\nCost: " + idleClickerUpgradeCost + "shdrudels\nPower: +1 shdrudel/s\nLevel: " + idleClickerUpgrade;
+        _idleClickerText.text = "Passive Upgrade numer 1\nCost: " + passiveUpgradeCost + "shdrudels\nPower: +1 shdrudel/s\nLevel: " + passiveUpgrade;
 
-        shdrudel += shtrudalpersecond * Time.deltaTime;
+        shdrudel += shtrudalPassive * Time.deltaTime;
 
     }
 
 
     public void BuyIdle()
     {
-        if (shdrudel >= idleClickerUpgradeCost)
+        if (shdrudel >= passiveUpgradeCost)
         {
-            idleClickerUpgrade++;
-            shdrudel -= idleClickerUpgradeCost;
-            idleClickerUpgradeCost *= 1.07;
+            passiveUpgrade++;
+            shdrudel -= passiveUpgradeCost;
+            passiveUpgradeCost *= 1.05;
         }
         
 
